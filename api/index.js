a51640e3d455f2edf44d84c16f72d615e3d1647d
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-12-05 09:34:38 +0100

    finished with backend part - rewrote the previous Vigenere homework as per the new requirements

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+app.use(express.json());
+
+app.post('/encode', (req, res) => {
+    const {password, message} = req.body;
+    if (!password || !message) {
+        return ('Password and message are required');
+    }
+    const cipheredText = Vigenere.Cipher(password).crypt(message);
+    return res.json({encoded: cipheredText});
+});
+
+app.post('/decode', (req, res) => {
+    const {password, message} = req.body;
+    if (!password || !message) {
+        return ('Password and message are required');
+    }
+    const decipheredText = Vigenere.Decipher(password).crypt(message);
+    return res.json({decoded: decipheredText});
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
